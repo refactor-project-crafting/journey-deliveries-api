ca27d8ee4eb0b39e0fde01b15adf03582d34d40b
Lorena Martínez <lmartmol@gmail.com>
2024-04-01 18:03:58 +0200

    Format without single quotes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
   getPong(): string {
-    return 'pong 🏓';
+    return "pong 🏓";
   }
 }
